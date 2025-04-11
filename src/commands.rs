b4db4c5575731f83c10295fa731de6d0c858fd8d
Travis Bubb <travisbubb14@gmail.com>
2025-04-11 19:47:48 -0400

    feat: Add simple command-line argument parsing using clap

---
+++ src/commands.rs
@@ -0,0 +1,11 @@
+pub fn init() {
+    println!("Initializing new RustyPages project...");
+}
+
+pub fn build() {
+    println!("Building the site...");
+}
+
+pub fn clean() {
+    println!("Cleaning output directory...");
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,32 @@
+use clap::Subcommand;
+use clap::{Parser, command};
+
+#[derive(Parser)]
+#[command(name = "RustyPages")]
+#[command(about = "A lightweight static site generator")]
+#[command(version, about, long_about = None)]
+struct Args {
+    #[command(subcommand)]
+    command: Option<Commands>,
+}
+
+#[derive(Subcommand)]
+enum Commands {
+    /// Initializes a new RustyPages site
+    Init,
+    /// Build the site into static files
+    Build,
+    /// Clean the generated output
+    Clean,
+}
+
 fn main() {
-    rustypages::run();
+    let args = Args::parse();
+
+    match args.command {
+        Some(Commands::Init) => rustypages::commands::init(),
+        Some(Commands::Build) => rustypages::commands::build(),
+        Some(Commands::Clean) => rustypages::commands::clean(),
+        None => println!("Unknown command. Please run 'rustypages help' to see available commands"),
+    };
 }
